eb1ad1e7011dee7f00495b9f1194e1b2ee96438e
shenwanxiang <shenwanxiang@tsinghua.org.cn>
2020-01-08 18:30:40 +0800

    update

---
+++ chembench/__init__.py
@@ -0,0 +1,32 @@
+import os
+from glob import glob
+import pandas as pd
+
+curr_path = os.path.dirname(__file__)
+
+def load_data(task_name):
+    '''
+    parameters
+    -------------
+        task_name: 'ESOL','FreeSolv','Lipop','PDBbind-full', 'PDBbind-core', 'PDBbind-refined','PCBA', 'MUV', 'HIV', 'BACE', 'BBBP', 'Tox21', 'ToxCast','SIDER', 'ClinTox', 'ChEMBL'
+    
+    return 
+    --------
+        dataframe table and the split induces
+    '''
+    file = os.path.join(curr_path, 'data_and_index', task_name, '%s.csv.gz' % task_name)
+    assert os.path.exists(file), 'no such file: %s !' % file_name
+    
+    pkl_file_match = os.path.join(curr_path, 'data_and_index', task_name, '*.pkl')
+    pklfiles = glob(pkl_file_match)
+    assert len(pklfiles) != 0, 'No induces pkl files !'
+    print( 'loading dataset: %s' % task_name, 'number of split times: %s' % (len(pklfiles)))
+    
+    induces = []
+    for pklf in pklfiles:
+        s = pd.read_pickle(pklf)
+        induces.append(s.to_list())
+        
+    df = pd.read_csv(file, compression = 'gzip')
+    return df, induces
+
